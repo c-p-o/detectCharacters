5b8cd3741c7fd1202d2df9ef35afbae8d8d9bee3
Christopher Otto <revengeofthefuture@gmail.com>
2021-08-25 18:27:24 -0400

    Initial Commit

---
+++ detectCharacters/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  detectCharacters
+//
+//  Created by Christopher Otto on 8/25/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
